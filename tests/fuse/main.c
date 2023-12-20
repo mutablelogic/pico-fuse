e11303f54520e6f0654ae1dd97c8cdf9a9cc29d8
David Thorpe <djt@mutablelogic.com>
2023-12-20 08:38:46 +0100

    Updated tests

--- tests/fuse/main.c
+++ tests/fuse/main.c
@@ -139,6 +139,154 @@ int TEST_007()
     return fuse_destroy(app);
 }
 
+int TEST_008()
+{
+    fuse_t *app = fuse_new();
+
+    // Create a list value
+    fuse_value_t *scalar = fuse_value_new_bool(app);
+    assert(scalar != NULL);
+    fuse_value_retain(app, scalar);
+
+    // Debug
+    char buffer[32];
+    assert_cstr_eq("false", fuse_value_cstr(app, scalar, buffer, 32));
+
+    // Free memory
+    fuse_debugf("Releasing a scalar value\n");
+    fuse_value_release(app, scalar);
+
+    // Release resources
+    return fuse_destroy(app);
+}
+
+int TEST_009()
+{
+    fuse_t *app = fuse_new();
+
+    // Create a list value
+    fuse_value_t *scalar = fuse_value_new_s8(app);
+    assert(scalar != NULL);
+    fuse_value_retain(app, scalar);
+
+    // Debug
+    char buffer[32];
+    assert_cstr_eq("0", fuse_value_cstr(app, scalar, buffer, 32));
+
+    // Free memory
+    fuse_debugf("Releasing a scalar value\n");
+    fuse_value_release(app, scalar);
+
+    // Release resources
+    return fuse_destroy(app);
+}
+
+
+int TEST_010()
+{
+    fuse_t *app = fuse_new();
+
+    // Create a list value
+    fuse_value_t *scalar = fuse_value_new_s16(app);
+    assert(scalar != NULL);
+    fuse_value_retain(app, scalar);
+
+    // Debug
+    char buffer[32];
+    assert_cstr_eq("0", fuse_value_cstr(app, scalar, buffer, 32));
+
+    // Free memory
+    fuse_debugf("Releasing a scalar value\n");
+    fuse_value_release(app, scalar);
+
+    // Release resources
+    return fuse_destroy(app);
+}
+
+int TEST_011()
+{
+    fuse_t *app = fuse_new();
+
+    // Create a list value
+    fuse_value_t *scalar = fuse_value_new_s32(app);
+    assert(scalar != NULL);
+    fuse_value_retain(app, scalar);
+
+    // Debug
+    char buffer[32];
+    assert_cstr_eq("0", fuse_value_cstr(app, scalar, buffer, 32));
+
+    // Free memory
+    fuse_debugf("Releasing a scalar value\n");
+    fuse_value_release(app, scalar);
+
+    // Release resources
+    return fuse_destroy(app);
+}
+
+int TEST_012()
+{
+    fuse_t *app = fuse_new();
+
+    // Create a list value
+    fuse_value_t *scalar = fuse_value_new_s64(app);
+    assert(scalar != NULL);
+    fuse_value_retain(app, scalar);
+
+    // Debug
+    char buffer[32];
+    assert_cstr_eq("0", fuse_value_cstr(app, scalar, buffer, 32));
+
+    // Free memory
+    fuse_debugf("Releasing a scalar value\n");
+    fuse_value_release(app, scalar);
+
+    // Release resources
+    return fuse_destroy(app);
+}
+
+int TEST_013()
+{
+    fuse_t *app = fuse_new();
+
+    // Create a list value
+    fuse_value_t *scalar = fuse_value_new_f32(app);
+    assert(scalar != NULL);
+    fuse_value_retain(app, scalar);
+
+    // Debug
+    char buffer[32];
+    assert_cstr_eq("0", fuse_value_cstr(app, scalar, buffer, 32));
+
+    // Free memory
+    fuse_debugf("Releasing a scalar value\n");
+    fuse_value_release(app, scalar);
+
+    // Release resources
+    return fuse_destroy(app);
+}
+
+int TEST_014()
+{
+    fuse_t *app = fuse_new();
+
+    // Create a list value
+    fuse_value_t *scalar = fuse_value_new_f64(app);
+    assert(scalar != NULL);
+    fuse_value_retain(app, scalar);
+
+    // Debug
+    char buffer[32];
+    assert_cstr_eq("0", fuse_value_cstr(app, scalar, buffer, 32));
+
+    // Free memory
+    fuse_debugf("Releasing a scalar value\n");
+    fuse_value_release(app, scalar);
+
+    // Release resources
+    return fuse_destroy(app);
+}
+
 int main()
 {
     assert(TEST_001() == 0);
@@ -148,6 +296,13 @@ int main()
     assert(TEST_005() == 0);
     assert(TEST_006() == 0);
     assert(TEST_007() == 0);
+    assert(TEST_008() == 0);
+    assert(TEST_009() == 0);
+    assert(TEST_010() == 0);
+    assert(TEST_011() == 0);
+    assert(TEST_012() == 0);
+    assert(TEST_013() == 0);
+    assert(TEST_014() == 0);
 
     // Return success
     return 0;
